571505957f01a83a201970634bdc8a7b85ba708b
Manju Raghavendra Bellamkonda <raghavb509@gmail.com>
2018-10-01 14:24:54 -0400

    Making call to backend api

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,17 +5,23 @@ import { MainComponent } from './main.component';
 import { MainAuthResolver } from './main-auth-resolver.service';
 import { HomeModule } from '../home/home.module';
 import { MainRoutingModule } from './main-routing.module';
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   imports: [
     HomeModule,
-    MainRoutingModule
+    MainRoutingModule,
+    HttpClientModule,
+    CommonModule
   ],
   declarations: [
     MainComponent
   ],
   providers: [
-    MainAuthResolver
+    MainAuthResolver,
+    HttpClient
   ]
 })
 export class MainModule {}
